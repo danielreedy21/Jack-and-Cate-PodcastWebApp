00e6721159b64a85a104da1913f85d581e4e304a
danielreedy21 <dreedy@luc.edu>
2022-10-11 20:00:44 -0500

    Updated upper nav bar

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-'strict mode';
+'use strict';
+
+var clicks = 0;
+
+document.getElementById("testButton").addEventListener("click", onClick);
+
+function onClick() {
+    console.log("CLicked!")
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
