c8cd1432b64344c93e435bf0c212a6c767a8e56d
danielreedy21 <dreedy@luc.edu>
2022-10-17 15:50:02 -0500

    Rough outline of episodes page complete

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 var clicks = 0;
 
-document.getElementById("testButton").addEventListener("click", onClick);
 
-function onClick() {
-    console.log("CLicked!")
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
-}
+// const testButton = document.getElementById("testButton");
+// const header = document.getElementById("mainHeader");
+
+// testButton.addEventListener("click", onClick);
+
+
+// function onClick() {
+//     console.log("CLicked!")
+//     clicks += 1;
+//     document.getElementById("clicks").innerHTML = clicks;
+// }
