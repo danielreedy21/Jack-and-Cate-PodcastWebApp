01d6b2e8a41594543331bacd898e852e8b09acad
danielreedy21 <dreedy@luc.edu>
2022-10-29 16:14:01 -0500

    Cleaned up CSS with variables, additional styling for navbar icons

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 'use strict';
 
-var clicks = 0;
 
 
-// const testButton = document.getElementById("testButton");
-// const header = document.getElementById("mainHeader");
 
-// testButton.addEventListener("click", onClick);
-
-
-// function onClick() {
-//     console.log("CLicked!")
-//     clicks += 1;
-//     document.getElementById("clicks").innerHTML = clicks;
-// }
