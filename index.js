db931228edeb97f386e5c85f020d78c98062136e
danielreedy21 <dreedy@luc.edu>
2022-11-20 20:46:26 -0600

    got audio pulling from firebase

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js';
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js';
 import { getStorage, ref, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +21,9 @@ const firebaseConfig = {
   measurementId: "G-DVFPMGBT2X"
 };
 
+
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -29,11 +32,37 @@ const db = getFirestore(app);
 
 
 
-//grab latest image url from firebase
+// TODO: grab the title and description information for the latest episode and add it to the DOM
+const querySnapshot = await getDocs(collection(db, "EpisodesData"));
+querySnapshot.forEach((doc) => {
+	const id = doc.id;
+	const data = JSON.stringify(doc.data(), undefined, 2);
+	console.log(id + ' => ' + data);
+});
+
+
+
+
 // Get the download URL and add it to the thumbnail src attribute
-const thumbRef = ref(storage, 'thumbnails/Forest_in_Japan.jpeg');
+const thumbRef = ref(storage, 'thumbnails/Episode Thumbnails/concert_thumb.jpeg');
 getDownloadURL(thumbRef)
     .then((url) => {
         const latestThumb = document.getElementById('latestThumb');
         latestThumb.src = url;
-    })
+    });
+
+// grab the audio from firebase and add it to the DOM
+const latestAudioRef = ref(storage, 'audio/ConcertPodcast.mp3');
+getDownloadURL(latestAudioRef)
+	.then((url) => {
+		const latestAudio = document.getElementById('latestAudio');
+		latestAudio.src = url;
+	});
+
+
+
+
+// TODO: grab the LATEST EPISODES section thumbnails and titles and add them to the DOM
+
+
+
